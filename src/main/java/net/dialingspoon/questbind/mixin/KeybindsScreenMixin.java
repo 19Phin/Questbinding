b4d23de13171dabd1596ab7b88c6814c850186b2
19Phin <19phin@gmail.com>
2024-04-23 12:56:14 -0700

    1.20.4

--- src/main/java/net/dialingspoon/questbind/mixin/KeybindsScreenMixin.java
+++ src/main/java/net/dialingspoon/questbind/mixin/KeybindsScreenMixin.java
@@ -3,6 +3,7 @@ package net.dialingspoon.questbind.mixin;
 import net.dialingspoon.questbind.interfaces.MinecraftClientInterface;
 import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.option.ControlsListWidget;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.screen.option.KeybindsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
@@ -22,17 +23,20 @@ public class KeybindsScreenMixin extends GameOptionsScreen {
 
 	@Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/option/KeybindsScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;"))
 	private Element addDrawableChildRedirect(KeybindsScreen instance, Element element) {
-        //custom done button
-        //!reset
-        ButtonWidget buttonWidget = (ButtonWidget) element;
+		if (!(element instanceof ControlsListWidget)) {
+		//custom done button
+		//!reset
+		ButtonWidget buttonWidget = (ButtonWidget) element;
 		if (buttonWidget.getMessage() == ScreenTexts.DONE) {
-			return this.addDrawableChild(ButtonWidget.builder(ScreenTexts.DONE, button -> {
+			return this.addDrawableChild(ButtonWidget.builder(ScreenTexts.DONE, (button) -> {
 				//update the binds when screen is closed
-				((MinecraftClientInterface)client).getKeyBindUtil().actualSave();
+				((MinecraftClientInterface) client).getKeyBindUtil().actualSave();
 				this.client.setScreen(this.parent);
 			}).dimensions(buttonWidget.getX(), buttonWidget.getY(), buttonWidget.getWidth(), buttonWidget.getHeight()).build());
 		}
-
 		return this.addDrawableChild(buttonWidget);
+		} else {
+			return this.addDrawableChild((ControlsListWidget)element);
+		}
 	}
 }
