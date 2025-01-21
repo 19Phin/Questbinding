c093034589e426bf28092803500b098e3bcb490e
19Phin <19phin@gmail.com>
2025-01-21 10:55:07 -0700

    bug

--- src/main/java/net/dialingspoon/questbind/mixin/KeybindsScreenMixin.java
+++ src/main/java/net/dialingspoon/questbind/mixin/KeybindsScreenMixin.java
@@ -4,7 +4,7 @@ import net.dialingspoon.questbind.interfaces.MinecraftClientInterface;
 import net.dialingspoon.questbind.util.KeyBindUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.ControlsListWidget;
@@ -12,6 +12,7 @@ import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.screen.option.KeybindsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -55,11 +56,11 @@ public class KeybindsScreenMixin extends GameOptionsScreen {
 	}
 
 	@Inject(method = "render", at = @At(value = "TAIL"))
-	protected void renderWarning(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+	protected void renderWarning(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
 		TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
 
 		//draw the warning on the screen (centered)
 		String message = "Must restart Questcraft for changes to take place!";
-		context.drawCenteredTextWithShadow(textRenderer, message, (this.width) / 2, 22, 0xFF0000);
+		DrawableHelper.drawCenteredTextWithShadow(matrices, textRenderer, message, (this.width) / 2, 22, 0xFF0000);
 	}
 }
